54866bffe29127469353286a2ffbbe9c45fef68a
jinho4785 <jinho4785@gmail.com>
2025-11-12 21:09:18 +0900

    or 연산자

--- src/NumberCounter.js
+++ src/NumberCounter.js
@@ -3,24 +3,18 @@ import React, { useState } from "react";
 function NumberCounter() {
   const [no, setNo] = useState(0);
 
-  const noIsEvenDiv =
-    no % 2 == 0 ? (
-      <>
-        <hr />
-        <span>짝수입니다.</span>
-      </>
-    ) : (
-      <></>
-    );
-
-  const noIs8MultipleDiv = no % 8 == 0 && (
+  const noIsEvenDiv = (
     <>
-      <hr />
-      <span>8의 배수입니다.</span>
+      <hr /> {no % 2 == 0 ? <div>짝수입니다.</div> : <div>홀수입니다.</div>}
     </>
   );
 
-  console.log(`noIs8MultipleDiv : ${noIs8MultipleDiv}`);
+  const noIs8MultipleDiv = no % 8 == 0 || (
+    <>
+      <hr />
+      <div>8의 배수가 아닙니다.</div>
+    </>
+  );
 
   return (
     <div>
