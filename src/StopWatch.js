e04611bab88f8fb6acecee4a7c1bc46c9a94e468
jinho4785 <jinho4785@gmail.com>
2025-11-12 20:09:58 +0900

    일시정지

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -3,10 +3,19 @@ import React, {useState} from "react";
 function StopWatch() {
   const [num, setNum] = useState(0);
 
-  setTimeout(()=>setNum(num + 1), 1000);
+  const timeoutId = setTimeout(()=>setNum(num + 1), 1000);
+  
+  const pause = () => {
+    console.log("실행됨!");
+    clearTimeout(timeoutId);
+  }
+  
   return (
     <div>
       숫자 : {num}
+
+      <hr />
+      <button onClick={pause}>일시정지</button>
     </div>
   );
 }
