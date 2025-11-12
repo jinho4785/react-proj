6dcfedbbb44f0823c5646bdd9de4f267c7277edc
jinho4785 <jinho4785@gmail.com>
2025-11-12 20:52:57 +0900

    8의 배수일 경우 문구가 나오도록

--- src/App.js
+++ src/App.js
@@ -9,6 +9,6 @@ const App = () => {
       {<NumberCounter />}
     </div>
   );
-}
+};
 
 export default App;
--- src/NumberCounter.js
+++ src/NumberCounter.js
@@ -3,15 +3,15 @@ import React, { useState } from "react";
 function NumberCounter() {
   const [no, setNo] = useState(0);
 
-  const noIsEvenDiv = 
+  const noIsEvenDiv =
     no % 2 == 0 ? (
       <>
         <hr />
         <span>짝수입니다.</span>
-      </> 
-    ) : ( 
+      </>
+    ) : (
       <></>
-  );
+    );
 
   return (
     <div>
@@ -19,6 +19,14 @@ function NumberCounter() {
       <hr />
       <button onClick={() => setNo(no + 1)}>증가</button>
       {noIsEvenDiv}
+      {no % 8 == 0 ? (
+        <>
+          <hr />
+          <span>8의 배수입니다.</span>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
